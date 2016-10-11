e5c103eddf9fcf7c6d32f96640d271dbb18184d3
Adin Kwok <adin.kwok@gmail.com>
2016-10-11 16:11:47 -0700

    include NexusLauncherPrebuilt as default launcher
    Signed-off-by: Adin Kwok <adin.kwok@gmail.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -20,6 +20,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/custom/prebuilt/etc/init.custom.rc:root/init.custom.rc
 
+PRODUCT_COPY_FILES += \
+    vendor/custom/prebuilt/apps/NexusLauncherPrebuilt/NexusLauncherPrebuilt.apk:system/priv-app/NexusLauncherPrebuilt/NexusLauncherPrebuilt.apk
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/custom/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
